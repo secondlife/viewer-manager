ca912bed3e9478ef3c80d2451cada0c2b9469d18
Glenn Glazer <coyot@lindenlab.com>
2017-01-27 14:52:40 -0800

    SL-621: split args, echo cmd directly

--- build-cmd.py
+++ build-cmd.py
@@ -114,12 +114,12 @@ def main():
         #In a typical Windows install, pyinstaller lives in C:\PythonXX\Scripts\pyinstaller.exe where Scripts is a sibling of the python executable
         #BUT that's not true of the virtualenv that autobuild runs in, so hard code the canonical location
         pyinstaller_exe = [r'C:\Python27\Scripts\pyinstaller']
-        args = [ "-y", "-windows", "--clean", "--onefile", "--log-level DEBUG", "-p " + iter_paths[key]['dst'], "--distpath " + iter_paths[key]['dst']]
+        args = [ "-y", "-windows", "--clean", "--onefile", "--log-level DEBUG", "-p ",  "iter_paths[key]['dst']", "--distpath ", "iter_paths[key]['dst']"]
         for f in vmp_files:
             try:
                 target = []
                 target.append(f)
-                print "about to call %s using %s on %s" % (pyinstaller_exe, args, target)
+                print "about to call %s " % (pyinstaller_exe + args + target)
                 subprocess.check_call(pyinstaller_exe + args + target)
             except Exception as e:
                 print "Pyinstaller failed"
