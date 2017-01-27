1a330480a6ddbbc697a12b3fb76002a63f7d7955
Glenn Glazer <coyot@lindenlab.com>
2017-01-27 09:58:48 -0800

    SL-621: fixed missed refactored variable

--- build-cmd.py
+++ build-cmd.py
@@ -107,9 +107,9 @@ def main():
         #to keep things as platform independent as possible, EXEs go into the same directory as .py files
         p = re.compile(r"\.py$")
         vmp_files = []
-        for f in os.listdir(dest):
+        for f in os.listdir(iter_paths[key]['dst']):
             if p.search(f):
-                vmp_files.append(str(os.path.join(dest, f)))
+                vmp_files.append(str(os.path.join(iter_paths[key]['dst'], f)))
         #exe's for those that don't
         #In a typical Windows install, pyinstaller lives in C:\PythonXX\Scripts\pyinstaller.exe where Scripts is a sibling of the python executable
         pyinstaller_exe = os.path.join(os.path.dirname(sys.executable), 'Scripts/pyinstaller.exe')
