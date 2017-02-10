25a400d69a5f37d3ffe9cd2a2e1aaeacfe6bea6e
Glenn Glazer <coyot@lindenlab.com>
2017-02-10 12:33:19 -0800

    SL-621: bail out early if pyinstaller is missing on windows, better logging of copytree, ignore std libs if they come from a different path than python - e.g., in virtualenvs

--- build-cmd.py
+++ build-cmd.py
@@ -74,7 +74,7 @@ def main():
     os.chdir(iter_paths['vmp']['src'])
     try:
         print "About to call %s on %s from %s" % (nosetest_cmd, tests, iter_paths['vmp']['src'])
-        subprocess.check_output([nosetest_cmd, tests])
+        subprocess.check_output([nosetest_cmd, tests], stderr=subprocess.STDOUT)
     except Exception as e:
         print repr(e)
         try:
@@ -97,6 +97,7 @@ def main():
         if os.path.exists(iter_paths[key]['dst']):
             rmtree(iter_paths[key]['dst'], ignore_errors=True)
         copytree(iter_paths[key]['src'], iter_paths[key]['dst'], ignore=ignore_patterns('*.pyc', '*tests*'))
+        print "copied %s to %s with contents %s" % (iter_paths[key]['src'], iter_paths[key]['dst'], repr(os.listdir(iter_paths[key]['dst'])))
 
     #remove dummy JSON file used during unit tests
     #do this after copy because deleting from source makes the unit tests non-reusable
@@ -123,11 +124,13 @@ def main():
         #BUT that's not true of the virtualenv that autobuild runs in, so hard code the canonical location
         pyinstaller_exe = [r'C:\Python27\Scripts\pyinstaller.exe']
         args = [ "-y", "-w", "--clean", "--onefile", "--log-level", "DEBUG", "-p", iter_paths[key]['dst'], "--distpath", iter_paths[key]['dst']]
+        print "pyinstaller exists: %s" % os.path.exists(pyinstaller_exe[0])
+        if not os.path.exists(pyinstaller_exe[0]):
+            sys.exit(1)
         for f in vmp_files:
             try:
                 target = []
                 target.append(f)
-                print "pyinstaller exists: %s" % os.path.exists(pyinstaller_exe[0])
                 print "target exists: %s" % os.path.exists(target[0])
                 print "about to call %s " % (pyinstaller_exe + args + target)
                 subprocess.check_output(pyinstaller_exe + args + target)
@@ -153,5 +156,6 @@ def main():
         
 if __name__ == '__main__':
     #trace is used as the pythonic equivalent of set -x in build_cmd.sh files, to produce output for TeamCity logs.
-    tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix], ignoremods=["subprocess"], trace=1, count=0, timing=True)
+    libs = os.path.dirname(shutil.__file__)
+    tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix, libs], ignoremods=["subprocess"], trace=1, count=0, timing=True)
     tracer.run('main()')
