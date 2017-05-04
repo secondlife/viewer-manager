9a9c8a618cc17ae86e380a04590827d6ec5c9bf3
Oz Linden <oz@lindenlab.com>
2017-05-04 15:07:23 -0400

    turn off timestamps on trace output; don't need them locally, and TeamCity already has them for each line

--- build-cmd.py
+++ build-cmd.py
@@ -209,5 +209,5 @@ def main():
 if __name__ == '__main__':
     #trace is used as the pythonic equivalent of set -x in build_cmd.sh files, to produce output for TeamCity logs.
     libs = os.path.dirname(os.__file__)
-    tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix, libs], ignoremods=["subprocess", "shutil"], trace=1, count=0, timing=True)
+    tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix, libs], ignoremods=["subprocess", "shutil"], trace=1, count=0, timing=False)
     tracer.run('main()')
