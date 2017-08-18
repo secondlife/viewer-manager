536d2231ccb1e12cb12dc56dfa14b528fa25ae46
Nat Goodspeed <nat@lindenlab.com>
2017-08-18 13:54:43 -0400

    MAINT-7661: Now that NSIS puts the .winstall marker, skip in VMP.

--- vmp-src/apply_update.py
+++ vmp-src/apply_update.py
@@ -282,9 +282,9 @@ def apply_windows_update(installable):
         ctypes.windll.shell32.ShellExecuteW(None, u'runas', unicode(installable), "", None, 1)
 
     #in the Windows case, we launch NSIS and never get a return to check
-    #assume that if we got this far, NSIS succeeds
-    #put a marker in the dir to signal to update manager to rm the directory on the next run
-    put_marker_file(download_dir, ".winstall")
+    #NSIS will put a marker in the dir to signal to update manager to rm the
+    #directory on the next run
+##  put_marker_file(download_dir, ".winstall")
 
     # Tell SL_Launcher not to run the old viewer -- the installer is still
     # running, and presumably will continue running for some time to come.
