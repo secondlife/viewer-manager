52705efeae4c28abeee214449b84560f8d6afc96
Nat Goodspeed <nat@lindenlab.com>
2018-09-13 16:09:43 -0400

    DRTVWR-474: Make updater support precheck, leap subcommands.
    Resurrect the old VITA leap.py module; make the leap subcommand import that,
    which engages preliminary LEAP protocol handling.

--- src/updater.py
+++ src/updater.py
@@ -23,12 +23,57 @@ if __name__ == '__main__':
 class Error(Exception):
     pass
 
-# called with the arguments we should pass to the viewer,
-# the first of which is the viewer executable itself
-def main(*args):
-    # Happily, that means 'args' is already a suitable subprocess command.
+# This subcommand is typically invoked by the Windows NSIS installer upon
+# successful installation. It is passed the arguments we should pass to
+# the viewer, the first of which is the viewer executable itself.
+def precheck(viewer, args):
     # Don't wait for the viewer to terminate, return immediately.
-    subprocess.Popen(args)
+    subprocess.Popen([viewer] + list(args))
+
+# This subcommand is typically invoked by the viewer itself to check for
+# updates during a run.
+def leap():
+    # Defer importing leap module because its module-level initialization
+    # expects LEAP protocol data on sys.stdin
+    import leap as _leap
+    print >>sys.stderr, "reply = {!r}".format(_leap.replypump())
+    print >>sys.stderr, "cmd   = {!r}".format(_leap.cmdpump())
+    print >>sys.stderr, "Done"
+
+def main(*raw_args):
+    from argparse import ArgumentParser, REMAINDER
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(title='subcommands',
+        description='Use %(prog)s <subcommand> --help for help on any subcommand')
+
+    # precheck subcommand
+    subprecheck = subparsers.add_parser('precheck',
+        help="""Check for update before the first viewer run. If a
+        cross-address-size update is required, download and install the
+        appropriate viewer. Otherwise, run the specified viewer with the
+        specified command-line arguments.""")
+    subprecheck.add_argument('viewer',
+        help="""Full pathname of the viewer to run""")
+    # REMAINDER means anything else on the command line, such as viewer
+    # switches -- even viewer switches starting with - or --
+    subprecheck.add_argument('args', nargs=REMAINDER,
+        help="""command-line arguments to pass to 'viewer'""")
+    subprecheck.set_defaults(func=precheck)
+
+    # leap subcommand
+    subleap = subparsers.add_parser('leap',
+        help="""Check for updates as a LEAP viewer child process""")
+    subleap.set_defaults(func=leap)
+
+    # Parse the command line and invoke appropriate subcommand.
+    args = parser.parse_args(raw_args)
+    argvars = vars(args)
+    # We forced 'func' into each subcommand parser, but no need to pass each
+    # function its own identity.
+    func = argvars.pop('func')
+    # This formulation requires that our subparser add_argument() calls match
+    # each function's parameter signature -- which is a Good Thing.
+    return func(**argvars)
 
 if __name__ == "__main__":
     try:
