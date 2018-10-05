1589be64595aff0555e80f3c4e9f465b8467de3d
Nat Goodspeed <nat@lindenlab.com>
2018-10-05 15:16:18 -0400

    DRTVWR-474: Flesh out leap() update logic.
    Engage new ViewerClient class to mediate communications with the parent
    viewer.
    
    Call selected update_manager functions to determine whether an update is
    necessary -- and, based on the user's preferences and whether the viewer has
    already logged in, what to do about it.

--- src/updater.py
+++ src/updater.py
@@ -28,7 +28,16 @@ from SL_Launcher import capture_vmp_args
 from runner import PopenRunner
 from InstallerUserMessage import status_message, root
 import update_manager
+from leapcomm import ViewerClient, RedirectUnclaimedReqid
 from tkeventlet import TkGreenthread
+
+# These definitions depend on the viewer's llstartup.h: if EStartupState
+# changes, so must they. Much as we would prefer to rely on string names
+# alone, we must divide the viewer session into "before clicking Login" vs.
+# "after clicking Login", which requires an inequality comparison on the enum
+# value.
+STATE_LOGIN_WAIT = 3
+STATE_WORLD_INIT = 8
 
 class Error(Exception):
     pass
@@ -93,17 +102,17 @@ def precheck(log, viewer, args):
 # ****************************************************************************
 # This subcommand is typically invoked by the viewer itself to check for
 # updates during a run.
-def leap(install_mode, channel, testok, vvmurl, width):
+def leap(install_key, channel, testok, vvmurl, width):
     """
     Pass:
-    install_mode: one of the values from the UpdaterServiceSetting combo_box
-    channel:      the viewer's actual channel name, as possibly overridden
-                  from the viewer command line
-    testok:       the viewer's actual UpdaterWillingToTest setting, as
-                  possibly overridden from the viewer command line
-    vvmurl:       the URL scheme://hostname/update prefix for the Viewer Version
-                  Manager to query (from UpdaterServiceURL)
-    width:        the ForceAddressSize setting
+    install_key: one of the numeric values from the UpdaterServiceSetting combo_box
+    channel:     the viewer's actual channel name, as possibly overridden
+                 from the viewer command line
+    testok:      the viewer's actual UpdaterWillingToTest setting, as
+                 possibly overridden from the viewer command line
+    vvmurl:      the URL scheme://hostname/update prefix for the Viewer Version
+                 Manager to query (from UpdaterServiceURL)
+    width:       the ForceAddressSize setting
     """
     params = locals().items()
     params.sort()
@@ -113,9 +122,9 @@ def leap(install_mode, channel, testok, vvmurl, width):
     # timestamping each line.
     log = SL_Logging.set_stream(sys.stderr, 'updater', SL_Logging.get_verbosity(),
                                 formatter=SL_Logging.TimelessFormatter())
-    # Defer importing leap module because its module-level initialization
-    # expects LEAP protocol data on sys.stdin
-    import leap as _leap
+    # This is where we engage LEAP protocol communications, processing the
+    # viewer's initialization data.
+    viewer = ViewerClient()
 
     platform_key = Application.platform_key() # e.g. "mac"
 
@@ -123,7 +132,227 @@ def leap(install_mode, channel, testok, vvmurl, width):
     for var, value in params:
         log.info("{} {!r}".format(var.ljust(varwidth), value))
 
-    log.info("Done")
+    install_mode = update_manager.decode_install_mode(install_key)
+
+    result = update_manager.query_vvm(platform_key=platform_key,
+                                      channel=channel,
+                                      UpdaterWillingToTest=testok,
+                                      UpdaterServiceURL=vvmurl)
+    if not result:
+        log.info("No update.")
+        update_manager.cleanup_previous_download(platform_key)
+        return
+
+    result = update_manager.choose_update(platform_key, width, result)
+    if not result:
+        update_manager.cleanup_previous_download(platform_key)
+        return
+
+    log.debug("Chosen result: %s", result)
+
+    if not update_manager.check_install_privs():
+        return
+
+    #get download directory, if there are perm issues or similar problems, give up
+    try:
+        download_dir = update_manager.make_download_dir(result['version'])
+    except Exception as e:
+        log.error("Error trying to make download dir: %s: %s", e.__class__.__name__, e)
+        return
+
+    # determine if we've tried this download before
+    downloaded = update_manager.check_for_completed_download(download_dir, chosen_result['size'])
+
+    if result['required']:
+        log.info("Required update to %s version %s", result['platform'], result['version'])
+        if catch_viewer_before_login(viewer, result['version'], "PauseForUpdate"):
+            viewer.shutdown()
+            # TODO: Is this correct?? Shouldn't we check for partial download?
+            if downloaded is None:
+                # We haven't yet downloaded the required update -- do so right
+                # now, in the foreground, with a progress bar.
+                update_manager.download(url=result['url'],
+                                        version=result['version'],
+                                        download_dir=download_dir,
+                                        hash=result['hash'],
+                                        size=result['size'],
+                                        background=False)
+            # Presumably we've just downloaded the new installer.
+            # TODO: Wouldn't it make sense for check_for_completed_download()
+            # and download() to return the full pathname of the downloaded
+            # installer, and for install() to accept that?
+            runner = update_manager.install(command=[],
+                                            platform_key=platform_key,
+                                            download_dir=download_dir)
+            runner.run()
+
+        else:
+            # We did NOT catch the viewer before login.
+            if downloaded is None:
+                # At least download it before the next viewer run.
+                background_download("required", download_dir, result)
+
+        # Either way, done handling required update.
+        return
+
+    # Not a required install, therefore optional. Is the user willing to
+    # accept optional updates?
+    if 'Install_manual' == install_mode:
+        log.info("not installing optional update per UpdaterServiceSetting")
+        return
+
+    # Yes the user is willing to accept optional updates. Have we already
+    # downloaded this one?
+    if downloaded is None:
+        # no, download it
+        background_download("optional", download_dir, result)
+        # and that's all we can do for now, we'll check for completion next run
+        return
+
+    if downloaded == 'skip':
+        log.info("Skipping this update per previous choice. "
+                 "Delete the .skip file in %s to change this.", download_dir)
+        return
+
+    if downloaded in ('done', 'next'):
+        # found a completed previous download of this optional update
+        log.info("Found previously downloaded update in: %s", download_dir)
+        # It matters whether the user has clicked Login yet. If we've already
+        # logged in, just wait until next time.
+        # TODO: That means that a user who always clicks Login really quickly
+        # could go for several sessions before being prompted to install an
+        # already-downloaded optional update.
+        if viewer.startup_state.enum > STATE_LOGIN_WAIT:
+            log.info("User already clicked Login (%s, %s), deferring",
+                     viewer.startup_state.str, viewer.startup_state.enum)
+            return
+
+        # If we're still sitting at the login screen, produce a popup and
+        # then, once the user closes it, shut down the running viewer and
+        # install the new viewer.
+        if 'Install_automatically' == install_mode:
+            response = viewer.request(
+                pump="LLNotifications",
+                data=dict(op="requestAdd", name="OptionalUpdateReady",
+                          substitutions=dict(VERSION=result['version']),
+                          payload={}))
+            return
+
+        # 'Install_ask'
+        # ask the user what to do with the optional update
+        log.info("asking the user what to do with the update")
+        response = viewer.request(
+            pump="LLNotifications",
+            data=dict(op="requestAdd", name="PromptOptionalUpdate",
+                      substitutions=dict(VERSION=result['version']),
+                      payload={}))
+        # The response sent by LLNotifications (packaged as ['response']) is a
+        # dict with keys for every button name in the form, one of whose
+        # values is True. Trust that only one will be true.
+        update_action = next(key for key, value in response['response'].items()
+                             if value)
+        log.debug("Picked %s from %s", update_action, response['response'])
+
+        if update_action == "Yes":
+            log.info("User chose 'Install'")
+            runner = update_manager.install(command=[],
+                                            platform_key=platform_key,
+                                            download_dir=download_dir)
+            runner.run()
+            return
+
+        if update_action == "No":
+            log.info("User chose 'Skip'")
+            put_marker_file(download_dir, ".skip")
+            return
+
+        # Not Now
+        log.info("User chose 'Not Now'")
+        put_marker_file(download_dir, ".next")
+        return
+
+    # should never get here
+    log.error("Found nonempty download dir but no marker file. Check returned: %r",
+              downloaded)
+    return
+
+# ****************************************************************************
+#   catch_viewer_before_login()
+# ****************************************************************************
+@pass_logger
+def catch_viewer_before_login(log, viewer, version, notification):
+    log.info("Viewer in %s (%s)", viewer.startup_state.str, viewer.startup_state.enum)
+    # In what state is the viewer? It matters whether the user has clicked
+    # Login yet.
+    if viewer.startup_state.enum <= STATE_LOGIN_WAIT:
+        # User hasn't yet clicked Login. Pop up a modal viewer
+        # notification, pre-empting his/her ability to do that.
+        log.info("popping up %s", notification)
+        # No timeout; we're waiting for a human being. Patience.
+        response = viewer.request(
+            pump="LLNotifications",
+            data=dict(op="requestAdd", name=notification,
+                      substitutions=dict(VERSION=version),
+                      payload={}))
+        # Presumably it doesn't much matter what's in 'response' ...
+        log.info("User acknowledged")
+        return True
+
+    # User was too quick on the draw: s/he already clicked Login. Try
+    # to intercept at the point of login failure.
+    # We can't treat this as a strict request/response handshake; it's
+    # too timing-dependent. Login might succeed, in which case we
+    # won't get an ack at all -- we'll only find out when the
+    # startup_state changes to STATE_WORLD_INIT or beyond.
+    reqid = viewer.next_reqid()
+    # Temporarily hook on a funky WaitFor instance to capture our ack,
+    # if any. Since what we see next might be either our ack or a
+    # StartupState change, redirect the possible ack to the
+    # WaitForStartup queue. Use a priority that ensures this redirect
+    # will see the ack first.
+    with RedirectUnclaimedReqid(viewer.startupWait, viewer, 10, reqid):
+        # Now post to the rendezvous point.
+        log.info("Posting to LoginSync")
+        viewer.send(pump="LoginSync", data=dict(reqid=reqid))
+        # Monitor startupWait's queue. Though it seems imprudent to wait
+        # without a timeout, the viewer may actually end up prompting
+        # the user with PauseForUpdate -- and there's no telling how
+        # long it might take the user to acknowledge.
+        for event in viewer.startupWait.iterate():
+            if event.get("reqid") == reqid:
+                # Oh good, we caught the viewer before login.
+                log.info("got ack from viewer")
+                return True
+            # If the startup_state ever gets to STATE_WORLD_INIT or
+            # beyond, login is progressing -- we won't ever get an ack.
+            # startupWait updates viewer.startup_state before posting to
+            # its queue, so if what arrives on the queue is a StartupState
+            # update, we don't literally have to examine the queued event.
+            if viewer.startup_state.enum >= STATE_WORLD_INIT:
+                log.info("Viewer logging in anyway: %s (%s)",
+                         viewer.startup_state.str, viewer.startup_state.enum)
+                return False
+
+# ****************************************************************************
+#   background_download()
+# ****************************************************************************
+@pass_logger
+def background_download(log, which, download_dir, result):
+    log.info("Found %s update to version %s. Downloading in background to: %s",
+             which, result['version'], download_dir)
+    # Create and launch a background thread. Because we do NOT set this
+    # thread as daemon, the updater process won't terminate until the
+    # thread completes.
+    background = threading.Thread(
+        name="downloader",
+        target=update_manager.download,
+        kwargs=dict(url=result['url'],
+                    version=result['version'],
+                    download_dir=download_dir,
+                    hash=result['hash'],
+                    size=result['size'],
+                    background=True))
+    background.start()
 
 # ****************************************************************************
 #   main()
@@ -151,7 +380,7 @@ def main(*raw_args):
     # leap subcommand
     subleap = subparsers.add_parser('leap',
         help="""Check for updates as a LEAP viewer child process""")
-    subleap.add_argument('install_mode', type=int,
+    subleap.add_argument('install_key',
                          help='UpdaterServiceSetting value')
     subleap.add_argument('channel',
                          help='the running viewer\'s channel name')
